0d774940890d400c5b44c18f05be31826dc4dccf
Jannik Schäfer <me@jschaefer.io>
2021-12-01 10:28:38 +0100

    Day01 cleanup

--- day01/day01.go
+++ day01/day01.go
@@ -7,6 +7,25 @@ import (
     "strings"
 )
 
+func offSetCount(s int, offset int, list []int) int {
+    sum := 0
+    for i := s; i > s-offset; i-- {
+        sum += list[i]
+    }
+    return sum
+}
+
+func traverseAndCompare(groupSize int, list []int) int {
+    length := len(list)
+    c := 0
+    for i := groupSize; i < length; i++ {
+        if offSetCount(i, groupSize, list) > offSetCount(i-1, groupSize, list) {
+            c++
+        }
+    }
+    return c
+}
+
 func init() {
     orchestration.MainDispatcher.AddSolver("Day01", orchestration.NewSolver(func(data string, result *orchestration.Result) error {
         depths := make([]int, 0)
@@ -14,25 +33,8 @@ func init() {
             v, _ := strconv.Atoi(line)
             depths = append(depths, v)
         }
-        length := len(depths)
-
-        aCount := 0
-        for i := 1; i < length; i++ {
-            if depths[i] > depths[i-1] {
-                aCount++
-            }
-        }
-        result.AddResult(fmt.Sprintf("%d", aCount))
-
-        bCount := 0
-        for i := 3; i < length; i++ {
-            sumA := depths[i] + depths[i-1] + depths[i-2]
-            sumB := depths[i-1] + depths[i-2] + depths[i-3]
-            if sumA > sumB {
-                bCount++
-            }
-        }
-        result.AddResult(fmt.Sprintf("%d", bCount))
+        result.AddResult(fmt.Sprintf("%d", traverseAndCompare(1, depths)))
+        result.AddResult(fmt.Sprintf("%d", traverseAndCompare(3, depths)))
         return nil
     }))
 }
